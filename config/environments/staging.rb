72b5c6ec6f38983fae151b1ccafa6bb2e8c42f23
Jelle Vandebeeck <jelle@fousa.be>
2016-07-06 23:34:07 +0200

    Add basic devise setup.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'thai-ko-gym.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'thai-ko-gym-staging.herokuapp.com' }
 end
