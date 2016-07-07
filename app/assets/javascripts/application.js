4cac5417c096c96f8b9f58c82222e7cf60cfc72a
Jelle Vandebeeck <jelle@fousa.be>
2016-07-07 19:18:57 +0200

    Add a basic footer.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,10 @@
 //= require jquery
-//= require jquery_ujs
+//= require bootstrap
+//= require js.cookie.min
+//= require jquery.slimscroll
+
+//= require app
+//= require layout
+
 //= require turbolinks
 //= require_tree .
