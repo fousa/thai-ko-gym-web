e3596bc7e181647659d92e30b71249283f2e0d76
Jelle Vandebeeck <jelle@fousa.be>
2016-07-11 17:23:45 +0200

    Cleanup the current application code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def author_tag
+    link_to 'fousa', 'http://fousa.be', target: '_BLANK', title: "Crafting awesome (web)apps since 2005!"
+  end
+
+  def flash_tag
+    if flash[:alert]
+      content_tag 'div', class: 'alert alert-danger' do
+        content_tag 'span', flash[:alert]
+      end
+    elsif flash[:notice]
+      content_tag 'div', class: 'alert alert-success' do
+        content_tag 'span', flash[:notice]
+      end
+    end
+  end
+
 end
